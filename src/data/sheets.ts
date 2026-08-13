6cd0104b8106dca18c7de4bf375bf02632e93f68
Ryan Liu <147843022+rLiu1140@users.noreply.github.com>
2026-08-12 21:25:06 -0400

    Implemented sheets

---
+++ src/data/sheets.ts
@@ -0,0 +1,5 @@
+export const SHEETS = {
+  steeringWeightCalcs: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1MNwN38o0GZELkX5vzoeTKKBB4Hd9JcQ3guwUmUavXJg9c-Di2X7r7H1ZeVLy_23KnN9CLYNsiLeK/pub?gid=0&single=true&output=csv",
+  steeringTubeComparison: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6GjTFrFzTx7WKgMbyo6h4V-guvgqbPEtbkXvUvtTAKxzBQSRCafLvcsOPEPHgg8wvPACHba2UbHui/pub?gid=0&single=true&output=csv",
+  steeringPO: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRyqx9ahadSWSZkC0GO6n97VV68zDkbsh2mJhdqRFq0IepAAkPKbXfON86X1DPrFk2KwdUTBfB26HG2/pub?gid=0&single=true&output=csv",
+} as const;
